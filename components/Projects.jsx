bff46ea9a537eb3fb98db00cc28f7c61f48f663f
SamRoehrich <sam_roehrich@icloud.com>
2020-11-08 13:51:23 -0700

    added all the projects.

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -42,12 +42,56 @@ const projects = [
       },
     ],
   },
-  // {
-  //   id: 3,
-  //   name: "test3",
-  //   description: "lorem ipsum totum nuctum",
-  //   photos: ["one", "two", "three", "four"],
-  // },
+  {
+    id: 3,
+    name: "Soccer Tournament Shirt",
+    client: "UNC Chapel Hill Women’s Soccer",
+    marginTop: 50,
+    description:
+      "Lars Van Dam was a women’s soccer coach at UNC Chapel Hill who passed in 2019 due to complications with his ongoing cancer battle. Early in 2019, before his death, the women of the team wanted to celebrate him and his career by renaming their annual soccer tournament after him. This shirt was made quickly but lovingly so that Lars would receive one. We used the school colors and classic iconography to make a clean and timeless design that would continue to be used by the school.",
+    photos: [
+      {
+        location: "/LVD 2.jpg",
+        width: 400,
+        height: 400,
+      },
+    ],
+  },
+  {
+    id: 4,
+    name: "Fire Department Patch & Sticker",
+    client: "Sonoita-Elgin Fire Department",
+    marginTop: 50,
+    description:
+      "The SEFD in Southern Arizona has mascots for each shift. They represent the familial energy of fire department crews and are a way to show pride. The C-Shift wanted to create a custom patch and sticker for their crew that would showcase their mascot (the fighting cobras) and that would be highly visible and easy to translate into many different sizes.",
+    photos: [
+      {
+        location: "/snek-color.jpg",
+        width: 400,
+        height: 400,
+      },
+    ],
+  },
+  {
+    id: 5,
+    name: "Kickball Event",
+    client: "Youth Recovery Organization",
+    marginTop: 50,
+    description:
+      "(Text redacted for anonymity) A local recovery group was holding a kickball tournament in Yuma, AZ to raise funds for a large conference. We wanted to emulate an old town western/southwestern feel, but with a kickball twist. The hand drawn illustration allowed this design to stand out and leave room for enough text for event information, and a black and white alternative was presented for inexpensive printing options.",
+    photos: [
+      {
+        location: "/YUMA COLOR.svg",
+        width: 400,
+        height: 400,
+      },
+      {
+        location: "/YUMA BW with text_forsite.jpg",
+        width: 400,
+        height: 400,
+      },
+    ],
+  },
 ];
 
 const Projects = () => {
